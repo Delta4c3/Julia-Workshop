40ea044cf6b46bcc6eb14ba4833dfe85a6f21f71
Prashiv <prashivradia2@gmail.com>
2022-11-17 19:21:09 +0200

    Completed task 02

--- 02-variables.jl
+++ 02-variables.jl
@@ -12,9 +12,37 @@ x = 3.14
 typeof(x)
 
 # Boolean: Bool
+b = true
+typeof(b)
+
+# BenchmarkTools
+# DataFrames
+# Statistics
+
 
 # Strings: String
+my_str = "Welcome to Julia Con"
+typeof(my_str)
+
+my_char = 'c'
+typeof(my_char)
+
+my_unicode = 'α'
+typeof(my_unicode)
+
+# Unicode chars are \LaTex command
+# Ω \Omega
+# ω \omega
+# π \pi 
+# ℯ \euler 
+
 
 # typeof
 
+
 # methodswith
+# This loads from julia defaulted. 
+# It's something that allows us to see what functions 
+# can be used with this type 
+
+methodswith(String)
